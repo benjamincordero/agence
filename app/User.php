9f5590ad8b0ee9ca3653c72ec70bb5bcdf3473e8
Benjamin <ingbenjamincordero@gmail.com>
2017-11-09 11:15:26 -0400

    First Commit

---
+++ app/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use DB;
+class User extends Authenticatable
+{
+    use Notifiable;
+
+
+}
---
+++ resources/views/con_desempenho.blade.php
@@ -0,0 +1,53 @@
+@extends('layout')
+@section('content')
+
+    <div class="panel panel-default">
+
+        <div class="panel panel-heading" align="center">
+
+            @include('periodo')
+        </div>
+
+        <div class="panel-body">
+            <div class="col-md-8 col-md-offset-2">
+                <select multiple size="8" name="list1" id="consultores">
+                    @foreach($usuarios as $usuario)
+                        <option value="{{ $usuario->co_usuario }}">{{ $usuario->no_usuario }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+        </div>
+           
+        <div class="panel-footer" align="center">
+            <a href="#" onclick="relatorio()" class="btn btn-info"><i class="fa fa-th"></i> Relatorio</a>
+            <a href="#" onclick="grafico()" class="btn btn-warning"><i class="fa fa-bar-chart"></i> Grafico</a>
+            <a href="#" onclick="pizza()" class="btn btn-success"><i class="fa fa-pie-chart"></i> Pizza</a>
+        </div>
+
+
+    </div>
+
+    <div id="vista" class="col-md-4 col-md-offset-4">
+
+    </div>
+
+    <div id="relatorio">
+
+    </div>
+
+@endsection
+
+
+@section('css')
+    <style>
+    .panel, .panel-heading{
+        border-radius: 0;
+    }
+    .btn{
+        border-radius: 0;
+    }
+
+    </style>
+
+@endsection
